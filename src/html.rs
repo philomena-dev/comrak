7d3453f5670f3d669cd7aacc91056a12676e753b
Luna <cod7777@yandex.ru>
2024-10-30 17:33:57 -0400

    Add support for Philomena paragraph divs and imgspoiler spans

--- src/html.rs
+++ src/html.rs
@@ -751,6 +751,9 @@ fn render_image<T>(
         if context.options.render.figure_with_caption {
             context.write_str("<figure>")?;
         }
+        if context.options.extension.philomena {
+            context.write_str("<span class=\"imgspoiler\">")?;
+        }
         context.write_str("<img")?;
         render_sourcepos(context, node)?;
         context.write_str(" src=\"")?;
@@ -770,6 +773,9 @@ fn render_image<T>(
             context.escape(&nl.title)?;
         }
         context.write_str("\" />")?;
+        if context.options.extension.philomena {
+            context.write_str("</span>")?;
+        }
         if context.options.render.figure_with_caption {
             if !nl.title.is_empty() {
                 context.write_str("<figcaption>")?;
@@ -916,9 +922,15 @@ fn render_paragraph<T>(
     if !tight {
         if entering {
             context.cr()?;
-            context.write_str("<p")?;
-            render_sourcepos(context, node)?;
-            context.write_str(">")?;
+            if context.options.extension.philomena {
+                context.write_str("<div class=\"paragraph\"")?;
+                render_sourcepos(context, node)?;
+                context.write_str(">")?;
+            } else {
+                context.write_str("<p")?;
+                render_sourcepos(context, node)?;
+                context.write_str(">")?;
+            }
         } else {
             if let Some(parent) = node.parent() {
                 if let NodeValue::FootnoteDefinition(ref nfd) = parent.data().value {
@@ -928,7 +940,11 @@ fn render_paragraph<T>(
                     }
                 }
             }
-            context.write_str("</p>")?;
+            if context.options.extension.philomena {
+                context.write_str("</div>")?;
+            } else {
+                context.write_str("</p>")?;
+            }
             context.lf()?;
         }
     }
---
+++ src/tests/philomena.rs
@@ -0,0 +1,21 @@
+use super::*;
+
+#[test]
+fn paragraph_divs() {
+    html_opts!(
+        [extension.philomena],
+        "hello world",
+        "<div class=\"paragraph\">hello world</div>\n"
+    );
+}
+
+#[test]
+fn imgspoiler_spans() {
+    html_opts!(
+        [extension.philomena],
+        concat!("![full](http://example.com/example.png)"),
+        concat!(
+            "<div class=\"paragraph\"><span class=\"imgspoiler\"><img src=\"http://example.com/example.png\" alt=\"full\" /></span></div>\n"
+        ),
+    );
+}
